9a54b0a9612dbc6d37f3212e38a9564c41c2c581
Mário César Silva <mcgato33@hotmail.com>
2020-09-29 15:15:57 -0100

    interface UserList com <ListItem />

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
+import { ListItem, Button } from 'react-native-elements';
 
 import users from '../data/users';
 
-const UserList = () => {
-
-  function getUserItem({ item }){
-    return <Text>{item.id} - {item.name}</Text>
+const UserList = (props) => {
+  function getActions(user) {
+    return(
+      <>
+        <Button 
+          onPress={() => props.navigation.navigate('UserForm', user)}
+        />
+      </>
+    );
   }
+
+  function getUserItem({ item: user }){
+    return (
+      <ListItem 
+        bottomDivider
+        leftAvatar={{source: {uri: user.avatarUrl}}}
+        key={user.id}
+        title={user.name}
+        subtitle={user.email}
+        onPress={() => props.navigation.navigate('UserForm')}
+        rightElement={getActions()}
+      />
+    );
+  };
 
   return(
     <View>
