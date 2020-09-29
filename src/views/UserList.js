981dd402edb48fef85dd201e51ae6e1412706651
Mário César Silva <mcgato33@hotmail.com>
2020-09-29 18:29:49 -0100

    button  edit e delete

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
-import { ListItem, Button } from 'react-native-elements';
+import { View, FlatList, Alert } from 'react-native';
+import { ListItem, Button, Icon } from 'react-native-elements';
 
 import users from '../data/users';
 
 const UserList = (props) => {
+  function confirmUserDeletion(user) {
+    Alert.alert('Apagar Utilizador', 'Deseja apagar o utilizador?',
+      [
+        {
+          text: 'Sim',
+          onPress() {
+            console.warn("Apagado" + user.id);
+          }
+        },
+        {
+          text: 'Não'
+        }
+      ]
+    );
+  };
+
   function getActions(user) {
     return(
       <>
         <Button 
           onPress={() => props.navigation.navigate('UserForm', user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="#f4511e" />}
+        />
+        <Button 
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
         />
       </>
     );
-  }
+  };
 
   function getUserItem({ item: user }){
     return (
